8c4ae5535dbd0162d557fa380aca5ed8ce2c9065
mdriaz <riazmd582@gmail.com>
2021-07-05 20:24:53 +0600

    insert data

--- main.js
+++ main.js
@@ -3,13 +3,37 @@ const mdbClient = require('mongodb').MongoClient;
 
 let URL = "mongodb+srv://mdriaz:rGEdMnsrHkZw9Dr4@cluster0.4tyhn.mongodb.net?retryWrites=true&w=majority";
 
-mdbClient.connect(URL, {useUnifiedTopology : true},function (error) {
+mdbClient.connect(URL, {useUnifiedTopology : true},function (error, con) {
     if (error){
         console.log('Connection failed');
     } else {
-        console.log('Connection established')
+        console.log('Connection established');
+        InsertData(con);
     }
 })
+
+function InsertData(con) {
+    let mydb = con.db('school');
+
+    let myCollection = mydb.collection('students');
+
+    let data = {
+        name: 'mdriaz',
+        roll: '01',
+        class: 'Diplomat',
+        city: 'Bogura'
+    }
+
+    myCollection.insertOne(data, function (error){
+        if (error){
+            console.log('Data insert failed')
+        } else {
+            console.log('Data insert success')
+        }
+    });
+}
+
+
 
 const server = http.createServer(function (req, res) {
 
